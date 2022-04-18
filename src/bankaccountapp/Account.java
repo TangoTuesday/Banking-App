2f6164f04dfc911f336c676b9787cf828e873431
TangoTuesday <99271990+TangoTuesday@users.noreply.github.com>
2022-04-18 05:55:56 -0500

    second commit, for test

--- src/bankaccountapp/Account.java
+++ src/bankaccountapp/Account.java
@@ -3,13 +3,13 @@ package bankaccountapp;
 public abstract class Account implements IBaseRate {
 
 	// List common properties for savings and checking accounts
-	String name;
-	String ssn;
-	double balance;
-	
-	static int index = 10000;
-	String accountNumber;
-	double rate;
+	private String name;
+	private String ssn;
+	private double balance;
+
+	private static int index = 10000;
+	protected String accountNumber;
+	protected double rate;
 
 	// Constructor to set base properties and initialize the account
 	public Account(String name, String ssn, double initDeposit) {
@@ -17,35 +17,82 @@ public abstract class Account implements IBaseRate {
 		this.name = name;
 		this.ssn = ssn;
 		balance = initDeposit;
-		
-		
-		
+
 		// Set Account Number
 		index++;
 		this.accountNumber = setAccountNumber();
-		
-	}
-	
-	private String setAccountNumber() { 
-		
-		String lastTwoOfSSN = ssn.substring(ssn.length()-2, ssn.length());
-		int uniqueID = index;
-		int randomNumber =(int) (Math.random() * Math.pow(10, 3));
-		return lastTwoOfSSN + uniqueID + randomNumber;
-		
+
+		setRate();
+
 	}
 
-	// List common methods
-	public void showInfo() {
-		
-		System.out.println(
-				
-				"Name: " + name +
-				"\nAccount Number: " + accountNumber +
-				"\nBalance: $" + balance
-				
-				);
-		
+	public abstract void setRate();
+
+	private String setAccountNumber() {
+
+		String lastTwoOfSSN = ssn.substring(ssn.length() - 2, ssn.length());
+		int uniqueID = index;
+		int randomNumber = (int) (Math.random() * Math.pow(10, 3));
+		return lastTwoOfSSN + uniqueID + randomNumber;
+
 	}
-	
+
+	public void compound() {
+
+		double accruedInterest = balance * (rate / 100);
+		balance = balance + accruedInterest;
+		System.out.println("\nAccrued Interest: $" + accruedInterest + "\n");
+		printBalance();
+
+	}
+
+	// List common methods - transactions
+
+	public void deposit(double amount) {
+
+		balance = balance + amount;
+		System.out.println("Depositing $" + amount);
+		printBalance();
+
+	}
+
+	public void withDraw(double amount) {
+
+		balance = balance - amount;
+		System.out.println("Withdrawing $" + amount);
+		printBalance();
+
+	}
+
+	public void transfer(String toWhere, double amount) {
+
+		balance = balance - amount;
+		System.out.println("Transfering $" + amount + " to " + toWhere);
+		printBalance();
+
+	}
+
+	public void printBalance() {
+
+		System.out.println("Your Balance is now $" + balance);
+
+	}
+
+	public void showInfo() {
+
+		System.out.println(
+
+				"Name: " + name + "\nAccount Number: " + accountNumber + "\nBalance: $" + balance + "\nRate: " + rate
+						+ "%"
+
+		);
+
+	}
+
+	public void dividers() {
+
+		System.out.println("\n **********************************\n");
+
+	}
+
 }
--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,24 +1,53 @@
 package bankaccountapp;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BankAccountApp {
 
 	public static void main(String[] args) {
-		
-		Checking chkacc1 = new Checking("Tom Wilson", "123456758", 1500);
-		
-		Savings savacc1 = new Savings("Rich Lowe", "345675485", 2500);
-		
-		savacc1.showInfo();
-		
-		System.out.println("****************");
-		
-		chkacc1.showInfo();
-		
-		
-		// Read a CSV File then create new accounts based on that data 
-		
-		
-		
+
+		List<Account> accounts = new LinkedList<Account>();
+
+		// Read a CSV File then create new accounts based on that data
+		String file = "C:\\Users\\NewBankAccounts.csv";
+
+		List<String[]> newAccountHolders = utilities.CSV.read(file);
+
+		for (String[] accountHolder : newAccountHolders) {
+
+			String name = accountHolder[0];
+			String ssn = accountHolder[1];
+			String accountType = accountHolder[2];
+			double initDeposit = Double.parseDouble(accountHolder[3]);
+
+			if (accountType.equals("Savings")) {
+
+				accounts.add(new Savings(name, ssn, initDeposit));
+
+			}
+
+			else if (accountType.equals("Checking")) {
+
+				accounts.add(new Checking(name, ssn, initDeposit));
+
+			}
+
+			else {
+
+				System.out.println("ERROR READING ACCOUT TYPE");
+
+			}
+
+		}
+
+		for (Account acc : accounts) {
+
+			acc.showInfo();
+			acc.dividers();
+
+		}
+
 	}
-	
+
 }
